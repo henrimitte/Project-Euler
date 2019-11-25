042b27cc4bb92ae34ccc178afda08f656764c13d
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2019-11-25 14:12:18 -0300

    Pretty much the same shit...

--- problems/001 - multiples of 3 and 5.py
+++ problems/001 - multiples of 3 and 5.py
@@ -1,6 +1,6 @@
-sum = 0
+divs_list = []
 for c in range(1, 1000):
-    if (c % 3 == 0) or (c % 5 == 0):
-        sum += c
+    if c % 3 == 0 or c % 5 == 0:
+        divs_list.append(c)
 
-print(sum)
+print(sum(divs_list))
