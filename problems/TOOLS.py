28bbd6aa1f6c302d902ebee561f3bb65cc60f935
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2019-11-25 13:56:23 -0300

    Small changes.

--- problems/TOOLS.py
+++ problems/TOOLS.py
@@ -1,16 +1,26 @@
 from math import sqrt
 
-def sieve_of_eratosthenes(limit = 10000):
+
+def sieve_of_eratosthenes(limit = 100):
+    '''Return a list with all prime numbers below the limit.'''
+    limit = abs(limit)
+    if limit < 2:
+        return []
+
     max_num_to_check = int(sqrt(limit)) + 1
-    all_numbers = [x for x in range(2, limit)]
+    all_numbers = [x for x in range(3, limit, 2)] # Get only odd numbers
     index = 0
     while True:
         number_being_checked = all_numbers[index]
-        all_numbers = [x for x in all_numbers if x % number_being_checked != 0 or x == number_being_checked]
+        all_numbers = [x for x in all_numbers
+                       if x == number_being_checked
+                       or x % number_being_checked != 0]
         index += 1
         next_prime = all_numbers[index]
         if next_prime > max_num_to_check:
+            all_numbers.insert(0, 2) # Insert 2 at index 0, as 2 is the first prime
             return all_numbers
+
 
 def factorial(num):
     num = abs(num)
@@ -20,17 +30,19 @@ def factorial(num):
 
 
 def fib(f1 = 0, f2 = 1, limit = 100):
+    '''Return a list with the fibonacci sequence to the nth (limit) index'''
     index = 2
-    next = 0
     fib_seq = [f1, f2]
     while True:
         index += 1
-        next = f1 + f2
-        fib_seq.append(next)
-        f1 = f2
-        f2 = next
+        next_num = f1 + f2
+        fib_seq.append(next_num)
+        f1, f2 = f2, next_num
         if index is limit:
             return fib_seq
 
 def simple_combination(n, p):
-    return factorial(n) / (factorial(p) * factorial(n - p))
+    fact_n = factorial(n)
+    fact_p = factorial(p)
+    fact_np = factorial(n - p)
+    return fact_n / (fact_p * fact_np)
