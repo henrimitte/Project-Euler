e4d55179b1f7fa8195d1621aec77f9f75edfb11e
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2020-01-01 04:42:49 -0300

    Implemented the max_path_sum function.

--- problems/TOOLS.py
+++ problems/TOOLS.py
@@ -2,14 +2,14 @@ import string
 from math import sqrt
 
 
-def sieve_of_eratosthenes(limit = 100):
+def sieve_of_eratosthenes(limit=100) -> list:
     '''Return a list with all prime numbers below the limit.'''
     limit = abs(limit)
     if limit < 2:
         return []
 
     max_num_to_check = int(sqrt(limit)) + 1
-    all_numbers = [x for x in range(3, limit, 2)] # Get only odd numbers
+    all_numbers = [x for x in range(3, limit, 2)]  # Get only odd numbers
     index = 0
     while True:
         number_being_checked = all_numbers[index]
@@ -19,18 +19,18 @@ def sieve_of_eratosthenes(limit = 100):
         index += 1
         next_prime = all_numbers[index]
         if next_prime > max_num_to_check:
-            all_numbers.insert(0, 2) # Insert 2 at index 0, as 2 is the first prime
+            all_numbers.insert(0, 2)  # Insert 2 at index 0
             return all_numbers
 
 
-def factorial(num):
+def factorial(num: int) -> int:
     num = abs(num)
     if num in (0, 1):
         return 1
     return num * factorial(num - 1)
 
 
-def fib(f1 = 0, f2 = 1, limit = 100):
+def fib(f1=0, f2=1, limit=100) -> list:
     '''Return a list with the fibonacci sequence to the nth (limit) index'''
     index = 2
     fib_seq = [f1, f2]
@@ -43,14 +43,31 @@ def fib(f1 = 0, f2 = 1, limit = 100):
             return fib_seq
 
 
-def simple_combination(n, p):
+def simple_combination(n: int, p: int) -> float:
     fact_n = factorial(n)
     fact_p = factorial(p)
     fact_np = factorial(n - p)
     return fact_n / (fact_p * fact_np)
 
 
-def is_one_through_n_pandigital(number_to_check, n = 5):
+def is_one_through_n_pandigital(number_to_check: int, n: int) -> bool:
+    '''
+    Check if a number is pandigital from 1 to n.
+
+    Args:
+        number_to_check: an integer with no more then 9 digits.
+        n: the size of the pangidital number, from 1 to 9
+    '''
     number_to_check = ''.join(sorted(str(number_to_check)))
-    all_nums = string.digits[1:n + 1] # Concatenated string with numbers 1..n
+    all_nums = string.digits[1:n + 1]  # Concatenated string with numbers 1..n
     return number_to_check == all_nums
+
+
+def max_path_sum(triangle: list) -> int:
+    triangle.reverse()
+    for row_index, row in enumerate(triangle[1:]):
+        for num_index, num in enumerate(row):
+            left_sum = num + triangle[row_index][num_index]
+            right_sum = num + triangle[row_index][num_index + 1]
+            triangle[row_index + 1][num_index] = max(left_sum, right_sum)
+    return triangle[-1]
