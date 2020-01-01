5ec7742cc885b0936a22e2664515a55af7f1ad80
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2020-01-01 04:40:05 -0300

    Just syntax changes

--- problems/002 - even fibonacci numbers.py
+++ problems/002 - even fibonacci numbers.py
@@ -1,13 +1,10 @@
-def fib(limit = 1000):
+def fib(limit=1000):
     a, b, s = 1, 2, 2
     while True:
         nxt = a + b
         if nxt % 2 == 0:
             s += nxt
-        a = b
-        b = nxt
-        if a + b > limit:
+        a, b = b, nxt
+        if nxt > limit:
             break
     return s
-
-print(fib(4000000))
