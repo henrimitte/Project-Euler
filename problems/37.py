4d5106edd04cb5d7d5ac97378c3cf96226323e8c
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2021-11-19 23:25:52 -0300

    Problem 37 solved. Doesn't fit 1 minute rule. Needs refactor.

---
+++ problems/37.py
@@ -0,0 +1,34 @@
+from TOOLS import sieve_of_eratosthenes
+
+primes = sieve_of_eratosthenes(800000)
+
+
+def left_to_right(n):
+    n = str(n)
+    for a in range(1, len(n)):
+        if int(n[a:]) not in primes:
+            return False
+    return True
+
+
+def right_to_left(n):
+    n = str(n)
+    for a in range(1, len(n)):
+        if int(n[:-a]) not in primes:
+            return False
+    return True
+
+
+s = found = 0
+index = 5
+while found < 11:
+    p = primes[index]
+    lr = left_to_right(p)
+    rl = right_to_left(p)
+    if lr and rl:
+        print(p)
+        s += p
+        found += 1
+    index += 1
+
+print(s)
