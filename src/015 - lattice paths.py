ad3a8565d92511484d99f92c087d005867eb1977
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2021-12-05 20:22:58 -0300

    Problem 15 refactored. No relevant changes.

--- src/015 - lattice paths.py
+++
@@ -1,3 +0,0 @@
-from tools import simple_combination
-
-print(simple_combination(40, 20))
---
+++ src/pe_problem15.py
@@ -0,0 +1,9 @@
+from tools import simple_combination
+
+
+def solve():
+    print(simple_combination(40, 20))
+
+
+if __name__ == '__main__':
+    solve()
