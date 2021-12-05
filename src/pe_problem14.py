1083849c4818204b70852e973bb3b277ae3da7ae
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2021-12-05 20:21:01 -0300

    Problem 14 refactored. Runs in ~1 sec.

---
+++ src/pe_problem14.py
@@ -0,0 +1,29 @@
+values = {1: 1}
+
+
+def _count_chain(n: int):
+    if n in values.keys():
+        return values[n]
+
+    if n % 2 == 0:
+        values[n] = 1 + _count_chain(n // 2)
+    else:
+        values[n] = 2 + _count_chain((3 * n + 1) // 2)
+
+    return values[n]
+
+
+def solve():
+    start = 1000000 // 2
+    longest_chain = ans = 0
+
+    for c in range(start, 1000000):
+        if _count_chain(c) > longest_chain:
+            longest_chain = _count_chain(c)
+            ans = c
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    solve()
