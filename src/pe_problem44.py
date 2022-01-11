b556bf2492a946d9dfa3f914055c85b64ba12c38
Henrique Roberto Gattermann Mittelstaedt <henrique.roberto97@gmail.com>
2022-01-11 04:48:58 -0300

    Refactored to use the get_nth_s_gonal/is_s_gonal functions from tools.

--- src/pe_problem44.py
+++ src/pe_problem44.py
@@ -1,13 +1,8 @@
-from tools import get_nth_pentagonal
-
-
-def _is_pentagonal(n: int) -> bool:
-    e = ((24 * n + 1) ** 0.5 + 1) / 6
-    return e == int(e)
+from tools import get_nth_s_gonal, is_s_gonal
 
 
 def solve():
-    pentagonals = [get_nth_pentagonal(x) for x in range(1, 3001)]
+    pentagonals = [get_nth_s_gonal(5, x) for x in range(1, 3001)]
     not_found = True
     i = 0
     length = len(pentagonals)
@@ -18,7 +13,7 @@ def solve():
         while j < length and dif_not_found:
             q = pentagonals[j]
             s, d = p + q, abs(q - p)
-            if _is_pentagonal(s) and _is_pentagonal(d):
+            if is_s_gonal(5, s) and is_s_gonal(5, d):
                 dif_not_found = not_found = False
             j += 1
         i += 1
